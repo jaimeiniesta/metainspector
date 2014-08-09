f9fda07c196e47e362059d8662641977c793d049
bastien <bastien.donjon@free.fr>
2014-08-09 14:52:27 +0200

    remove caractere

--- lib/meta_inspector/parser.rb
+++ lib/meta_inspector/parser.rb
@@ -49,7 +49,7 @@ module MetaInspector
     
     # Return favicon url if exist
     def favicon
-      @favicon ||= URL.absolutify(parsed.xpath('//link[@rel="icon" or contains(@rel, "shortcut")]]')[0].attributes['href'].value, base_url) rescue nil
+      @favicon ||= URL.absolutify(parsed.xpath('//link[@rel="icon" or contains(@rel, "shortcut")]')[0].attributes['href'].value, base_url) rescue nil
     end
 
     # A description getter that first checks for a meta description and if not present will
