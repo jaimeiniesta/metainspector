17bfd6c73b770f21796f066a75c42ef631fc98b6
Miguel Rodrigues <miguel.srod@gmail.com>
2023-03-23 17:43:28 +0000

    Update faraday_redirect_options.rb

--- examples/faraday_redirect_options.rb
+++ examples/faraday_redirect_options.rb
@@ -11,13 +11,13 @@ puts "Using MetaInspector #{MetaInspector::VERSION}"
 url = ARGV[0] || (puts "Enter an url"; gets.strip)
 
 # redirect options to be passed along to Faraday::FollowRedirects::Middleware
-redirects_opts = { limit: 5 }
-
-# custom callback to handle the redirect links
-redirects_opts[:callback] = proc do |_old_response, new_response|
-  ip_address = Resolv.getaddress(new_response.url.host)
-  raise 'Invalid address' if IPAddr.new(ip_address).private?
-end
+redirects_opts = {
+  limit: 5,
+  callback: proc do |_old_response, new_response|
+    ip_address = Resolv.getaddress(new_response.url.host)
+    raise 'Invalid address' if IPAddr.new(ip_address).private?
+  end
+}
 
 begin
   page = MetaInspector.new(url, faraday_options: { redirect: redirects_opts })
